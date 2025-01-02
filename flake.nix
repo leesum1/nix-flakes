9b18b410d79b1cc50b56f120d62e224b68cf7be4
leesum <1255273338@qq.com>
2025-01-02 21:39:42 +0800

    update flow

--- flake.nix
+++ flake.nix
@@ -13,11 +13,8 @@
       ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in {
-      legacyPackages = forAllSystems (system:
-        import ./default.nix { pkgs = import nixpkgs { inherit system; }; });
 
       packages = forAllSystems (system:
-        nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v)
-        self.legacyPackages.${system});
+        import ./default.nix { pkgs = import nixpkgs { inherit system; }; });
     };
 }
