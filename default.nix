02ac8fc47957d729b269b70f746182758257738f
leesum <1255273338@qq.com>
2025-01-02 20:43:16 +0800

    update picker

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  picker = pkgs.callPackage ./pkgs/picker { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
---
+++ pkgs/picker/default.nix
@@ -0,0 +1,39 @@
+{ stdenv, fetchFromGitHub # 废话
+, cmake # 使用cmake进行构建
+, swig }:
+
+let
+  version = "1.0";
+  pname = "picker";
+  xcomm_src = fetchFromGitHub {
+    owner = "XS-MLVP";
+    repo = "xcomm";
+    rev = "master";
+    sha256 = "sha256-teb1+OF3sy662lOXOMXTv6DhrDFqxGKAGevrmSRdwKQ=";
+  };
+
+in stdenv.mkDerivation {
+  # inherit version pname ;
+  name = "${pname}-${version}";
+
+  # 从 GitHub 下载源代码
+  src = fetchFromGitHub ({
+    owner = "XS-MLVP";
+    repo = "picker";
+    rev = "master";
+    sha256 = "sha256-RThhq3NwxqK13tQa5lCfS1bYqMjFh0mcf9bPSkX7ca4=";
+  });
+
+  dontFixCmake = true;
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ swig ];
+
+  patchPhase = ''
+    # Create the destination directory: dependence/xcomm
+    mkdir -p dependence/xcomm
+
+    # Copy xcomm source
+    cp -r ${xcomm_src}/* dependence/xcomm
+  '';
+}
